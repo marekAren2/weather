1804c5c7d3e135e8716b5523d022e9ffc1adad29
marekAren2 <147280986+marekAren2@users.noreply.github.com>
2023-11-24 14:30:35 +0100

    staraFunkcja() czemu consolog z L: 36 sie wykonuje

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+// 11:16 import axios ale w Weather czemu ASK ?
 import logo from './logo.svg';
 import './App.css';
 import Weather from './Weather';
-
+//import weather.css ok 19:
+import "./Weather.css"
+// 30: po stylowaniu opis dzialania
 function App() {
   return (
     <div className="App">
@@ -9,5 +12,28 @@ function App() {
     </div>
   );
 }
+//8:10 komponent plik Weather
+//ternary 33: cisnienie przechodzimy do filtrowania powtorka przekazania danych WeatherItem tworzenie
+//3: zmiany zmiennych na props
+//kluczowanie ta m gdzie mapowanie musi byc
+//37: searchWeather
+//38:55 wywolanie search 40: stylowanie
+//40: h1 tytul
+// 40:44 kontener dodatkowy s search form szukaj etykieta ciekawos htmlFor nie for z js 
+// 42: stylowanie
+// 43: opis
+//43:40 funkcja filterWeatcher z pparametrem
+// 45:przekazanie funkcji search
+// 45:45 wazne przekazani
+// opis 46:
+// opis filtrowania
+// 47:40 filWeDa f
+//48: opis
+// -19 49:33 JS casesense male litery w obie strony male litery
+// 50: 38 do stanu z loga
+// 51: wazne o skracaniu
+// 51:35 wracanie 2 listy
+// 52:50 zmiana nazwy stanu 
+// 49:53 rozwiaanie
 
 export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,55 @@
+
+// zgodnie z dokumentacja axios 11:15
+import axios from "axios";
+// 11:16 import axios ale w Weather czemu ASK ?
+
+import { StrictMode, useEffect, useState } from "react";
+
+// 10: init
 const Weather = () => {
+    // 10:40 useState do tablicy .1
+    
+    const [weatherData, setWeatherData] = useState([]);
+    //uzywamy 2-go useeffect hooka kiedy sie zaladuje 11:42 odpowiada za kontrole cyklu zycia naszego komponentu
+    // 12:12 teoria: calback,tablica zaleznosci i ktorego stanu zmiana ma wywolac zmiane czy zawsze domyslnie 
+    // pusta tabl zaleznosci wywola tylko raz po zaladowaniu komponentu
+    //czy jak u nas na starcie
+    // dokoncz : >> od 16:39 v1 pogoda warszt<<
+    
+    //16:36 REFAKTOR POBIERANIA
+    //obsluga tego zapytania 16:48
+    
+    const getWeatherData = () => {
+        axios.get('https://danepubliczne.imgw.pl/api/data/synop').then(res => {
+            console.log("🚀 ~ file: Weather.js:25 ~ getWeatherData ~ res:", res)
+        })
+    }
+
+        const staraFunkcja = () => {
+            // useEffect(()=>{
+                // test czy tak dziala 12:23
+                //12:47 StrictMode z index.js powoduje double consol.log
+                console.log('tylko raz zaladowanie komponentu to samo co zamontowanie?');
+                // czemu u mnie 2x nie consologuje przez StrictMode?
+                //13:16 pobieranie wybieramy metode opst wysyl get pobieranie
+                // Api Json ? z opisu z sc: 3999 string czyli ma byc '' 13:40
+                // podobnie jak featch bez .then automat
+                axios.get('https://danepubliczne.imgw.pl/api/data/synop')
+                //odpowiedzia jest funkcja
+                .then(res=>{
+                // zobacz co odpowiedzia jest 14:45
+                console.log(res);
+                // zapytanie typu xhr czyli xml-http-request ;metoda get status 200 ok
+                // ja 
+                setWeatherData(res);
+                // nie wiem czy jest sens wyswietlac funkcje
+                console.log("🚀 ~ file: Weather.js:36 ~ useEffect ~ setWeatherData(res):", setWeatherData(res))
+                console.log('ale zmienna - obiekt ustawiony setterem to ok:', weatherData);
+                })
+            // },[])
+        }
+        //16:36 REFAKTOR POBIERANIA
+
     return(
         <div className="weather">
             pogoda
